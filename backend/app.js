8724b2ca83f3deb3f539db612c75acfe8969b94c
Sneha Parmar <parmarsneha789@gmail.com>
2026-03-26 19:21:47 +0530

    updated build issues

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(",") : "https://career-three-sooty.vercel.app"],
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
